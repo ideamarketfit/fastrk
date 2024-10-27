50ae07f26e41b40fcb5fb133c6a1a6e1ae9f65b7
zhangruinan <ruinan.zhang@icloud.com>
2024-10-27 13:39:04 -0700

    fix i18n for home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { HreflangTags } from '@/components/hreflang-tags';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <HreflangTags />
         <script
           defer
           data-domain="chatdiagram.com"
